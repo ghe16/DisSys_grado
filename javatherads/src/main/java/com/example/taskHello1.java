35c526e3d2652e0db46b7e2e7c4f558a636b0f4e
Gustavo Hernandez <tavo16@gmail.com>
2024-08-24 13:56:09 +0200

    subir tarea2

---
+++ javatherads/src/main/java/com/example/taskHello1.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class taskHello1 implements Runnable {   
+    public void run() {
+        Thread t = Thread.currentThread();
+        System.out.println(" Hi from thread " + t.getId());
+        System.out.println("THREAD name : " + t.getName());
+        System.out.println(" id : " + t.getId());
+        System.out.println(" Priority : " + t.getPriority());
+        System.out.println("State : " + t.getState());
+        System.out.println("Thread group: " + t.getThreadGroup().getName());
+        System.out.println("----------------");
+    }
+
+    public static void main(String[] args){
+        taskHello1 taskHello = new taskHello1();
+        Thread thread1 = new Thread(taskHello);
+        Thread thread2 = new Thread(taskHello);
+        thread1.run();
+        thread2.run();
+    }
+    
+}
